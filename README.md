969b1a359d602756c48be9b40ad004fe5a36582c
Ryan Burley <36254205+RyanBurley@users.noreply.github.com>
2023-09-25 20:22:39 +0930

    proof of concept

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sky Catalogue - constructing a sky catalgoue for the ADACS workshop
+
+This script generates a sky catalogue, and has additional code to plot the results.
+
+Usage: python sky_sim.py
+
+Output: a file called 'catalog.csv' with the requested catalogue as a CSV file.
+
+Developed as a demo for the ASA ECR Python Workshop, Melbourne 2023 (remote participant)
+
+Author: Ryan Burley
---
+++ mymodule/sky_sim/sky_sim.py
@@ -0,0 +1,36 @@
+import math as mh
+import random as rd
+
+def generate_star_positions(nsrc):
+    '''
+    Function to generate right ascension and declination coordinates in degrees
+    '''
+    # from wikipedia
+    r_asc = '00:42:44.3'
+    dec = '41:16:09'
+
+    day, minu, sec = dec.split(':')
+    dec = int(day)+int(minu)/60+float(sec)/3600
+
+    hour, minu, sec = r_asc.split(':')
+    r_asc = 15*(int(hour)+int(minu)/60+float(sec)/3600)
+    r_asc = r_asc/mh.cos(dec*mh.pi/180)
+
+    ras = []
+    decs = []
+    for _ in range(nsrc):
+        ras.append(r_asc + rd.uniform(-1,1))
+        decs.append(dec + rd.uniform(-1,1))
+
+    return ras, decs
+
+def write_positions(ras,decs,nsrc):
+    '''
+    Function to print star coordinates to a CSV file
+    '''
+    path = '/Users/Ryan/Software/ADACS_Workshop/sky_catalogue'
+    # now write these to a csv file for use by my other program
+    with open(path+'/data/output/catalogue.csv','w',encoding="utf8") as file:
+        print("id,ra,dec", file=file)
+        for i in range(nsrc):
+            print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=file)
